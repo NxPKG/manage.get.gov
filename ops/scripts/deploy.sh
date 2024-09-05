f4da1b1c0850a0e153213111b11e3c1dc6d741cd
NxPKG js <iconmamundentist@gmail.com>
2024-09-05 22:55:13 +0000

    update

---
+++ ops/scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Compile and collect static assets
+../ops/scripts/build.sh
+
+# Deploy to sandbox 
+cf target -o govdotbd -s $1
+cf push getgov-$1 -f ../ops/manifests/manifest-$1.yaml
+
+# migrations need to be run manually. Developers can use this command
+#cf run-task getgov-SANDBOXNAME --command 'python manage.py migrate' --name migrate
